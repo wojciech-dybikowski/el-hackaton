d950210a75e5a6deb02cf2c8c4deffa7d8d04483
wojciech-dybikowski <wojciech.dybikowski@elpassion.pl>
2021-11-19 18:55:35 +0100

    [feature] Song component

--- shared/components/Song/Song.tsx
+++ shared/components/Song/Song.tsx
@@ -1,18 +1,20 @@
-import { Row } from "shared/ui"
-import { Column } from "shared/ui"
+import { Row } from 'shared/ui';
+import { Column } from 'shared/ui';
 
-export const Song =() => {
-    return(
-        <section>
-            <Row>
-                <i className="fas fa-play"></i>  
-                <Column>
-                    <p> 1967</p>
-                    <h2>Nowhere to run</h2>
-                    <h3>Martha Reeves</h3>
-                </Column>
-                <i className="far fa-heart"></i>
-            </Row>
-        </section>
-    )
-}
+import styles from './Song.module.css';
+
+export const Song = () => {
+  return (
+    <section>
+      <Row className={styles.song}>
+        <i className={styles.icon + ' fas fa-play'} />
+        <Column>
+          <p> 1967</p>
+          <h2>Nowhere to run</h2>
+          <h3>Martha Reeves</h3>
+        </Column>
+        <i className="far fa-heart" />
+      </Row>
+    </section>
+  );
+};
--- shared/ui/Row/Row.tsx
+++ shared/ui/Row/Row.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import classNames from 'classnames';
+// import classNames from 'classnames';
 
-export const Row: React.FC<{ className?: string, style?: React.CSSProperties }> = ({
+export const Row: React.FC<{ className?: string; style?: React.CSSProperties }> = ({
   className,
   style,
   ...props
 }) => {
-  return <div style={style} className={classNames('flex flex-row',className)} {...props} />;
+  return <div style={style} className={'flex flex-row ' + className} {...props} />;
 };
